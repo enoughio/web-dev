e1f6461f73340385f29e4866f1f18dae40619d96
Ani <turboji007@gmail.com>
2024-10-03 21:30:37 +0530

    database revice

--- 8.3 Travel Tracker/solution3.js
+++ 8.3 Travel Tracker/solution3.js
@@ -5,12 +5,12 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
-var db = new pg.Client({
+const db = new pg.Client({
   user: "postgres",
   host: "localhost",
+  database: "world",
+  password: "123456",
   port: 5432,
-  password: "aniketaajj",
-  database: "world"
 });
 db.connect();
 
